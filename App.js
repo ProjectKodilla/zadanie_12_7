cea5793933f1cdd3bb74e0e216f837ff11c18dd5
Marcin Bogdanski <marcin@bogdanski.info>
2018-03-29 01:28:11 +0200

    finished

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
 // OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
+function randomString () {
+  var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split()
+	var str = '', i
 	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
+	  str += chars[Math.floor(Math.random() * chars.length)]
 	}
-	return str;
+  return str
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api'
+var myHeaders = {
+  'X-Client-Id': '2829',
+  'X-Auth-Token': '2d211b7feeccac6efa6246a4a95dd08d'
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+  // 'X-Client-Id': 'X-Client-Id',
+  // 'X-Auth-Token': 'X-Auth-Token'
+}
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+$.ajaxSetup({
+  headers: myHeaders
+})
 
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+$.ajax({
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function (response) {
+    setupColumns(response.columns)
+    }
+})
+
+function setupColumns (columns) {
+  columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name)
+        board.createColumn(col)
+        setupCards(col, column.cards)
+    })
+}
+
+function setupCards (col, cards) {
+  cards.forEach(function (card) {
+    var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id)
+    	col.createCard(card)
+  	})
+}
